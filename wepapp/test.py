530a40ff96da0aed8acc69a94842cf3a40664552
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2025-01-15 17:28:04 -0800

    add detection code to multi process code

--- wepapp/test.py
+++ wepapp/test.py
@@ -25,10 +25,50 @@ def record(q):
 
 def detect(notify_q, record_q):
     cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture("rtsp://rtspstream:775bd82e9bbedf71dcbc7ef60deaf484@zephyr.rtsp.stream/pattern")
+    
+    path = 'weaponresource/checkpoints_weapon/WeaponOct24_608_8K'
+    detect_weapon = tf.saved_model.load(path)
     
     while(True):        
-        success, frame = cap.read()
-        cv2.imshow("frame", frame)
+        success, image_mask1 = cap.read()
+            
+        image_data = cv2.resize(image_mask1, (608, 608))
+        image_data = image_data / 255.
+        image_data = image_data[np.newaxis, ...].astype(np.float32)
+
+        infer_weapon = detect_weapon.signatures['serving_default']
+
+        batch_data = tf.constant(image_data)
+        pred_bbox = infer_weapon(batch_data)
+
+        for key, value in pred_bbox.items():
+            boxes = value[:, :, 0:4]
+            pred_conf = value[:, :, 4:]
+
+        # run non max suppression on detections
+        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+            scores=tf.reshape(
+                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+            max_output_size_per_class=50,
+            max_total_size=50,
+            iou_threshold=0.5,
+            score_threshold=0.3
+        )
+
+        original_h, original_w, _ = image_mask1.shape
+        bboxes = utils.format_boxes(boxes.numpy()[0], original_h, original_w)
+
+        pred_bbox = [bboxes, scores.numpy()[0], classes.numpy()[0], valid_detections.numpy()[0]]
+
+        class_names = utils.read_class_names("./wepdata/classes/weapons.names")
+
+        allowed_classes = list(class_names.values())
+
+        image2 = utils.draw_bbox(image_mask1, pred_bbox, info = False, allowed_classes=allowed_classes)
+        
+        cv2.imshow('Webcam', image2)
         
         key = cv2.waitKey(1)
         if key == ord('q'):
@@ -63,77 +103,3 @@ if __name__ == "__main__":
     
     notify_p.join()
     record_p.join()
-
-# # cap = cv2.VideoCapture(0)
-# # cap = cv2.VideoCapture("rtsp://rtspstream:775bd82e9bbedf71dcbc7ef60deaf484@zephyr.rtsp.stream/pattern")
-
-# path = 'weaponresource/checkpoints_weapon/WeaponOct24_608_8K'
-# detect_weapon = tf.saved_model.load(path)
-
-# while True:
-#     success, image_mask1 = cap.read()
-    
-#     image_data = cv2.resize(image_mask1, (608, 608))
-#     image_data = image_data / 255.
-#     image_data = image_data[np.newaxis, ...].astype(np.float32)
-
-#     infer_weapon = detect_weapon.signatures['serving_default']
-
-#     batch_data = tf.constant(image_data)
-#     pred_bbox = infer_weapon(batch_data)
-
-#     for key, value in pred_bbox.items():
-#         boxes = value[:, :, 0:4]
-#         pred_conf = value[:, :, 4:]
-
-#     # run non max suppression on detections
-#     boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
-#         boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
-#         scores=tf.reshape(
-#             pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
-#         max_output_size_per_class=50,
-#         max_total_size=50,
-#         iou_threshold=0.5,
-#         score_threshold=0.3
-#     )
-
-#     original_h, original_w, _ = image_mask1.shape
-#     bboxes = utils.format_boxes(boxes.numpy()[0], original_h, original_w)
-
-#     pred_bbox = [bboxes, scores.numpy()[0], classes.numpy()[0], valid_detections.numpy()[0]]
-
-#     class_names = utils.read_class_names("./wepdata/classes/weapons.names")
-
-#     allowed_classes = list(class_names.values())
-
-#     image2 = utils.draw_bbox(image_mask1, pred_bbox, info = False, allowed_classes=allowed_classes)
-    
-#     cv2.imshow('Webcam', image2)
-#     if cv2.waitKey(1) == ord('q'):
-#         break
-
-# cap.release()
-# cv2.destroyAllWindows()
-
-# try:
-#     while True:
-#         # Read the input live stream
-#         ret, frame = cap.read()
-
-#         height, width, layers =  frame.shape
-#         frame = cv2.resize(frame, (width // 2, height // 2))
-
-#         cv2.imshow("test rtsp link", frame)
-
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             # print("test")
-#             break
-
-# except Exception as e:
-#     print("ERROR:", e)
-
-# # Main function
-# if __name__ == "__main__":
-#     # Release and close stream
-#     cap.release()
-#     cv2.destroyAllWindows()
